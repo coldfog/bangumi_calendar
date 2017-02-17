77109c14fa37cdd9cc58e5166f64278f77d7006b
Yuyao Feng <yyaofeng@gmail.com>
2017-02-17 11:58:39 +0800

    corresponding tab will be active when initializing

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import tornado.ioloop
 import tornado.web
 from tornado import gen
@@ -8,6 +9,8 @@ import time
 
 __author__ = 'fengyuyao'
 
+WEEKDAY_NAME = [u"星期日", u"星期一", u"星期二", u"星期三", u"星期四", u"星期五", u"星期六"]
+
 
 class MainHandler(tornado.web.RequestHandler):
     @gen.coroutine
@@ -17,22 +20,28 @@ class MainHandler(tornado.web.RequestHandler):
 
         # print res
         # self.write(str(res))
-        cur_date = datetime.datetime.now()
-        self.render("../templates/index.html", bangumi_info=res, cur_weekday=cur_date.weekday())
+        cur_weekday = datetime.datetime.now().weekday() + 1
+        # for Sunday, in database Sunday is 0
+        if cur_weekday == 7:
+            cur_weekday = 0
+        self.render("../templates/index.html",
+                    bangumi_info=res,
+                    cur_weekday=cur_weekday,
+                    WEEKDAY_NAME=WEEKDAY_NAME)
 
     @gen.coroutine
     def get_data(self):
         bangumi_info = {}
 
         for i in range(7):
-            bangumi_info['day%d' % i] = []
-        bangumi_info['day%d' % -1] = []
+            bangumi_info[i] = []
+        bangumi_info[-1] = []
 
         bots = [bangumi_bot.TudouBot(), bangumi_bot.BilibiliBot()]
         for bot in bots:
             data = yield bot.get_data()
             for cur_record in data:
-                info_in_day = bangumi_info['day%d' % cur_record['weekday']]
+                info_in_day = bangumi_info[cur_record['weekday']]
 
                 for iRecord in info_in_day:
                     if self._bangumi_similar(iRecord, cur_record) > 0.5:
@@ -45,7 +54,7 @@ class MainHandler(tornado.web.RequestHandler):
         raise gen.Return(bangumi_info)
 
     def _bangumi_similar(self, a, b):
-         return difflib.SequenceMatcher(a=a['title'], b=b['title']).ratio()
+        return difflib.SequenceMatcher(a=a['title'], b=b['title']).ratio()
 
 
 def make_app():
